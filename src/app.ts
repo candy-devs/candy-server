fced276ebcaefe796fb03c5fd574b35b41e1ec90
rollrat <rollrat.cse@gmail.com>
2022-04-06 22:15:20 +0900

    express: add cors, cookie-parser, etc

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import createError from "http-errors";
 import express, { Request, Response, NextFunction } from "express";
 import { sequelize } from "./models";
 import { User } from "./models/user";
@@ -9,10 +12,20 @@ const HOST: string = process.env.HOST || "localhost";
 
 const app = express();
 
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use(routes);
+
+app.use(function (err: Error, req: Request, res: Response, next: NextFunction)  {
+  next(createError(404));
+});
 
 app.listen(PORT, HOST, () => {
   console.log(`server start ${HOST}:${PORT}`);
 });
 
-User.sync({force:true});
+// User.sync({ force: true });
