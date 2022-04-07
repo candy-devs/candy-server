ca8238211593b114afeb8dac628477f3460b4c0a
rollrat <rollrat.cse@gmail.com>
2022-04-07 21:23:56 +0900

    models: add UserAttributesUpdate interface

--- models/user.ts
+++ models/user.ts
@@ -10,6 +10,11 @@ interface UserAttributes {
   permission: number;
 }
 
+// https://joshua1988.github.io/ts/usage/mapped-type.html#%EB%A7%B5%EB%93%9C-%ED%83%80%EC%9E%85-%EC%8B%A4%EC%9A%A9-%EC%98%88%EC%A0%9C-2
+export type UserAttributesUpdate = {
+  [P in keyof UserAttributes]?: UserAttributes[P];
+}
+
 export class User extends Model<UserAttributes> {
   public user_id!: string;
   public password!: string;
