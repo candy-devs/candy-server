1828a77f4a1a0a016eecf0eab4b12b1cba690d59
rollrat <rollrat.cse@gmail.com>
2022-04-13 23:22:49 +0900

    routes: read article with user joined

--- routes/api/v1/article/read.ts
+++ routes/api/v1/article/read.ts
@@ -1,15 +1,21 @@
 import express, { Request, Response, NextFunction } from "express";
 import { Article } from "../../../../models/article";
+import { User } from "../../../../models/user";
 
 const router = express.Router();
 
-router.get("/read/:id", async (req: Request, res: Response, next: NextFunction) =>  {
-  const article = await Article.findByPk(req.params.id);
-  if (article == null) {
-    res.status(404).send();
-  } else {
-    res.type('json').send(article.toJSON());
+router.get(
+  "/read/:id",
+  async (req: Request, res: Response, next: NextFunction) => {
+    const article = await Article.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ["user_id"] }],
+    });
+    if (article == null) {
+      res.status(404).send();
+    } else {
+      res.type("json").send(article.toJSON());
+    }
   }
-});
+);
 
 export const routes = router;
