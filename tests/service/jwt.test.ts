dfa0e950004e762d27c9d2a370dfb39201b3f429
rollrat <rollrat.cse@gmail.com>
2022-04-16 13:48:58 +0900

    test: test jwt refresh token also

--- tests/service/jwt.test.ts
+++ tests/service/jwt.test.ts
@@ -4,7 +4,12 @@ import { createJWT } from "../../session/createJWT";
 it("Create JWT And Verify", () => {
   const jwtId = 123456789;
   const jwtRaw = createJWT(jwtId);
-  const jwtAccess = JSON.parse(jwtRaw)['accessToken'];
+
+  const jwtParsed = JSON.parse(jwtRaw);
+
+  const jwtAccess = jwtParsed['accessToken'];
+  const jwtRefresh = jwtParsed['refreshToken'];
 
   expect(verifyJWT('jwt-' + jwtAccess)).not.toBeNull();
+  expect(verifyJWT('jwt-' + jwtRefresh)).not.toBeNull();
 });
